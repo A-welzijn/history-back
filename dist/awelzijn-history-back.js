a012c96d46250d609d80bebface3c7f653b3e630
ex02153 <willem.denys@digipolis.be>
2015-06-16 11:27:22 +0200

    no message

---
+++ dist/awelzijn-history-back.js
@@ -0,0 +1,27 @@
+'use strict';
+(function (module) {
+  try {
+    module = angular.module('awelzijn.historyback');
+  } catch (e) {
+    module = angular.module('awelzijn.historyback', []);
+  }
+  module.directive('aWelzijnHistoryBack', ['$window', function ($window) {
+    return {
+      restrict: 'E',
+      replace: true,
+      link: function ($scope) {
+
+        $scope.terug = function () {
+          $window.history.back();
+        }
+      },
+      template:
+      '<div ng-click="terug()" class="back-btn">' +
+      '<p>' +
+      '<i class="fa fa-angle-left"></i>' +
+      '</p>' +
+      '</div>',
+    };
+  }]);
+})();
+;
